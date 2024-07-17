b63a213df14b8de0cea68a80f7bf50e078aeafc9
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2024-07-18 00:31:07 +0300

    newserver works, connection works

--- main.go
+++ main.go
@@ -1,9 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
 type Server struct {
 	conns map[*websocket.Conn]bool
+
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client: ", ws.RemoteAddr())
+
+	//add mutex for some reason
+	s.conns[ws] = true
+	s.readLoop(ws)
+}
+
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Println(string(msg))
+
+		ws.Write([]byte("thanks for the message"))
+	}
+	
 }
 
 func main() {
-
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.ListenAndServe(":3000", nil)
 }
